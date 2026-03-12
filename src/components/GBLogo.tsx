afb8cf2ab17511c97958c60f2e3dc3c4faed0bd2
Diogo Candeias <diogo.t.candeias@gmail.com>
2026-03-12 13:22:11 +0000

    fix logo use img tag

--- src/components/GBLogo.tsx
+++ src/components/GBLogo.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
-
 interface GBLogoProps {
   size?: number
 }
 
 export default function GBLogo({ size = 40 }: GBLogoProps) {
   return (
-    <Image
-      src="/Gracie Barra.png"
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src="/gb-logo.png"
       alt="Gracie Barra Carnaxide e Queijas"
       width={size}
       height={size}
